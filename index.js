bc94a9ea3af2313a3d506b671b84e579e609e19b
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2018-09-30 16:20:45 -0700

    DB

--- index.js
+++ index.js
@@ -12,4 +12,6 @@ app.post('/createUser', (req, res) => {
     })
     .then(() => res.sendStatus(200))
 })
-app.listen(3000, () => console.log('Server running on port 3000'))
+  app.listen(3000, () =>{
+    console.log('Server running on port 3000')
+})
