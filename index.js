c89c2fd7146d6b302534e68ace84545b04b86ba5
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2018-09-30 02:15:07 -0700

    add html

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+#setup to run HTML code from Public
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
