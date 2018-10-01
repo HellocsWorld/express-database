f1993d6df5fe4e2de4b12a6bbff16b674759957e
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2018-09-30 17:11:40 -0700

    here

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 module.exports = {
   client: 'mysql',
   connection: {
+    host: '34.217.62.9',
+    port: '3306',
     user: 'root',
     password: 'BzX3J#RgczvFxK3$',
     database: 'team-01-DB'
